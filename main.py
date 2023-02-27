6f0c943ecd2d12c5e4c30b1d72bda16e721788f6
jiangpenglong <jiangpenglong@huafagroup.com>
2023-02-27 10:02:47 +0800

    增加喜威

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from telethon.sync import TelegramClient
 
 import requests
 
-def get_JF():
+def hfvmall_sing_in():
     jf_url = 'https://wx.huafamall.cn/wxmall/wxsite/mbr/signin.do?wxpubid=gh_3e33c7377a96&wxopenid=oLVyYjuadRRvvzJ21X7JAYQDED_o'
     response = requests.get('https://wx.huafamall.cn/wxmall/wxsite/mbr/signin.do',{
         'wxpubid':'gh_3e33c7377a96',
@@ -40,9 +40,19 @@ def sendMessage():
     
     client.session.save()
 
+def xiwei_sign_in():
+    xiwei_url = 'https://zhongshan.xiweigas.com/api/mobile/user/userSignIn'
+    current_time = time.time()
+    cookies = "Hm_lpvt_c439c5adcd8614f603da7372e6c8017d="+str(current_time)+";Hm_lvt_c439c5adcd8614f603da7372e6c8017d="+str(current_time)+";SYSTEM_GLOBAL=uc5ud1fd7unkfnvcjamrb6a184"
+    cookies2 = dict(map(lambda x: x.split('='), cookies.split(";")))
+    response = requests.post(xiwei_url,cookies=cookies2)
+    resp = response.json()
+    print(resp['errmsg'])
+
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     sendMessage()
-    get_JF()
+    hfvmall_sing_in()
+    xiwei_sign_in()
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
